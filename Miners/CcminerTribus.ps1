6b701a7260ed36fb11f68ace324e5a6b3b9e3810
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2017-07-26 15:06:36 -0600

    Add files via upload

---
+++ Miners/CcminerTribus.ps1
@@ -0,0 +1,57 @@
+﻿$Path = '.\Bin\NVIDIA-TPtribus\ccminer-x64.exe'
+$Uri = 'https://github.com/tpruvot/ccminer/releases/download/v2.1-tpruvot/ccminer-2.1-tribus.7z'
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Algorithms = [PSCustomObject]@{
+    #Lyra2z = 'lyra2z'
+    #Equihash = 'equihash' #not supported
+    #Cryptonight = 'cryptonight'
+    #Ethash = 'ethash' #not supported
+    #Sia = 'sia'
+    #Yescrypt = 'yescrypt'
+    #BlakeVanilla = 'vanilla'
+    #Lyra2RE2 = 'lyra2v2'
+    #Skein = 'skein'
+    #Qubit = 'qubit'
+    #NeoScrypt = 'neoscrypt'
+    #X11 = 'x11'
+    #MyriadGroestl = 'myr-gr'
+    #Groestl = 'groestl'
+    #Keccak = 'keccak'
+    #Scrypt = 'scrypt'
+    Tribus = 'tribus'
+}
+
+$Optimizations = [PSCustomObject]@{
+    Lyra2z = ''
+    Equihash = ''
+    Cryptonight = ''
+    Ethash = ''
+    Sia = ''
+    Yescrypt = ''
+    BlakeVanilla = ''
+    Lyra2RE2 = ''
+    Skein = ''
+    Qubit = ''
+    NeoScrypt = ''
+    X11 = ''
+    MyriadGroestl = ''
+    Groestl = ''
+    Keccak = ''
+    Scrypt = ''
+    Tribus = ''
+}
+
+$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = 'NVIDIA'
+        Path = $Path
+        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -u $($Pools.', $_, '.User) -p $($Pools.', $_, '.Pass)', $Optimizations.$_)
+        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
+        API = 'Ccminer'
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+    }
+}
