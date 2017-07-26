796d0134896dc9927d75a481a4217c4729db7c9e
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2017-07-26 16:24:38 -0600

    Add files via upload

---
+++ Miners/Ccmineralexis.ps1
@@ -0,0 +1,77 @@
+﻿$Path = '.\Bin\NVIDIA-Alexis\ccminer-x64.exe'
+$Uri = 'https://github.com/nemosminer/ccminer-Alexis78/releases/download/ccminer-alexis78/ccminer-alexis78-ms2013-cuda7.5.7z'
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Algorithms = [PSCustomObject]@{
+    #Lyra2z = 'lyra2z'
+    #Equihash = 'equihash' #not supported
+    #Cryptonight = 'cryptonight'
+    #Ethash = 'ethash' #not supported
+    #Sia = 'sia'
+    #Yescrypt = 'yescrypt'
+    #BlakeVanilla = 'vanilla'
+    #Lyra2RE2 = 'lyra2v2'
+    #Skein = 'skein'
+    #Qubit = 'qubit'
+    #NeoScrypt = 'neoscrypt'
+    #X11 = 'x11'
+    #MyriadGroestl = 'myr-gr'
+    #Groestl = 'groestl'
+    #Keccak = 'keccak'
+    #Scrypt = 'scrypt'
+    #Bitcore = 'bitcore'
+    #Blake2s = 'blake2s'
+    #Sib = 'sib'
+    #X17 = 'x17'
+    #Quark = 'quark'
+    #Hmq1725 = 'hmq1725'
+    #Veltor = 'veltor'
+    #X11evo = 'x11evo'
+    #Timetravel = 'timetravel'
+    #Blakcoin = 'blakecoin'
+    Lbry = 'lbry'
+}
+
+$Optimizations = [PSCustomObject]@{
+    Lyra2z = ''
+    Equihash = ''
+    Cryptonight = ''
+    Ethash = ''
+    Sia = ''
+    Yescrypt = ''
+    BlakeVanilla = ''
+    Lyra2RE2 = ''
+    Skein = ''
+    Qubit = ''
+    NeoScrypt = ''
+    X11 = ''
+    MyriadGroestl = ''
+    Groestl = ''
+    Keccak = ''
+    Scrypt = ''
+    Bitcore = ''
+    Blake2s = ''
+    Sib = ''
+    X17 = ''
+    Quark = ''
+    Hmq1725 = ''
+    Veltor = ''
+    X11evo = ''
+    Timetravel = ''
+    Blakecoin = ''
+    Lbry = ''
+}
+
+$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = 'NVIDIA'
+        Path = $Path
+        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -u $($Pools.', $_, '.User) -p $($Pools.', $_, '.Pass)', $Optimizations.$_)
+        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
+        API = 'Ccminer'
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+    }
+}
