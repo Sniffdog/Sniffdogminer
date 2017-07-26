8c5942fbce59aaaeda9b25446d37417c5e1434e2
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2017-07-25 20:47:00 -0600

    Add files via upload

---
+++ Miners/CcminerKlaust.ps1
@@ -0,0 +1,57 @@
+﻿$Path = '.\Bin\NVIDIA-KlausT\ccminer.exe'
+$Uri = 'https://github.com/KlausT/ccminer/releases/download/8.11/ccminer-811-x64.zip'
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Algorithms = [PSCustomObject]@{
+    #Lyra2z = 'lyra2z' #not supported
+    #Equihash = 'equihash' #not supported
+    #Cryptonight = 'cryptonight' #not supported
+    #Ethash = 'ethash' #not supported
+    #Sia = 'sia' #use TpruvoT
+    #Yescrypt = 'yescrypt' #use TpruvoT
+    #BlakeVanilla = 'vanilla'
+    #Lyra2RE2 = 'lyra2v2' #use TpruvoT
+    #Skein = 'skein' #use TpruvoT
+    #Qubit = 'qubit' #use TpruvoT
+    #NeoScrypt = 'neoscrypt'
+    #X11 = 'x11' #use TpruvoT
+    MyriadGroestl = 'myr-gr'
+    Groestl = 'groestl'
+    #Keccak = 'keccak' #use TpruvoT
+    #Scrypt = 'scrypt' #use TpruvoT
+    Nist5 = 'nist5'
+}
+
+$Optimizations = [PSCustomObject]@{
+    Lyra2z = ''
+    Equihash = ''
+    Cryptonight = ''
+    Ethash = ''
+    Sia = ''
+    Yescrypt = ''
+    BlakeVanilla = ''
+    Lyra2RE2 = ''
+    Skein = ''
+    Qubit = ''
+    NeoScrypt = ''
+    X11 = ''
+    MyriadGroestl = ''
+    Groestl = ''
+    Keccak = ''
+    Scrypt = ''
+    Nist5 = ''
+}
+
+$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = 'NVIDIA'
+        Path = $Path
+        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -u $($Pools.', $_, '.User) -p $($Pools.', $_, '.Pass)', $Optimizations.$_)
+        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
+        API = 'Ccminer'
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+    }
+}
