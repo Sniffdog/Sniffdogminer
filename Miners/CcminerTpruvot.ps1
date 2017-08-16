fe26fa9cbb481499aee5d23e645f41142d8e0699
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2017-08-16 07:43:57 -0600

    Update CcminerTpruvot.ps1

--- Miners/CcminerTpruvot.ps1
+++ Miners/CcminerTpruvot.ps1
@@ -1,5 +1,5 @@
 ﻿$Path = '.\Bin\NVIDIA-TPruvot\ccminer-x64.exe'
-$Uri = 'https://github.com/tpruvot/ccminer/releases/download/v2.0-tpruvot/ccminer-2.0-release-x64-cuda-8.0.7z'
+$Uri = 'https://github.com/tpruvot/ccminer/releases/download/v2.2-tpruvot/ccminer-x64-2.2.7z'
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
