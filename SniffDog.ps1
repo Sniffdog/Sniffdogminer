127c378dc74af83cdbe558454f96e691c15e08e2
ahashpool <31766130+ahashpool@users.noreply.github.com>
2017-12-16 04:00:05 +0800

    Fix BTC-USD rate

--- SniffDog.ps1
+++ SniffDog.ps1
@@ -91,9 +91,10 @@ while($true)
         $LastDonated = Get-Date
     }
 
-    $Rates = [PSCustomObject]@{}
-    $Currency | ForEach {$Rates | Add-Member $_ (Invoke-WebRequest "https://api.cryptonator.com/api/ticker/btc-$_" -UseBasicParsing | ConvertFrom-Json).ticker.price}
-
+    Write-host "Loading BTC rate from 'api.coinbase.com'.." -foregroundcolor "Yellow"
+    $Rates = Invoke-RestMethod "https://api.coinbase.com/v2/exchange-rates?currency=BTC" -UseBasicParsing | Select-Object -ExpandProperty data | Select-Object -ExpandProperty rates
+    $Currency | Where-Object {$Rates.$_} | ForEach-Object {$Rates | Add-Member $_ ([Double]$Rates.$_) -Force}
+    
     #Load the Stats
     $Stats = [PSCustomObject]@{}
     if(Test-Path "Stats"){Get-ChildItemContent "Stats" | ForEach {$Stats | Add-Member $_.Name $_.Content}}
