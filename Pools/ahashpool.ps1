0521c8532faa157ea1bee99000efba04487510a2
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-08 08:06:57 -0700

    Update ahashpool.ps1

--- Pools/ahashpool.ps1
+++ Pools/ahashpool.ps1
@@ -27,9 +27,14 @@ $ahashpool_Request | Get-Member -MemberType NoteProperty | Select -ExpandPropert
     {
         "equihash"{$Divisor /= 1000}
         "blake2s"{$Divisor *= 1000}
-		"blakecoin"{$Divisor *= 1000}
+	"yescrypt"{$Divisor /= 1000}
+        "sha256"{$Divisor *= 1000}
+        "sha256t"{$Divisor *= 1000}
+        "blakecoin"{$Divisor *= 1000}
         "decred"{$Divisor *= 1000}
-        "x11"{$Divisor *= 100}
+        "keccak"{$Divisor *= 1000}
+        "keccakc"{$Divisor *= 1000}
+        "vanilla"{$Divisor *= 1000}
     }
 
     if((Get-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit") -eq $null){$Stat = Set-Stat -Name "$($Name)_$($ahashpool_Algorithm)_Profit" -Value ([Double]$ahashpool_Request.$_.estimate_last24h/$Divisor)}
