478775c569a7a48ad2bbf04bb82320572eb1dd01
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-19 22:24:13 -0600

    Add files via upload

---
+++ Miners/CcminerAlexis_78.ps1
@@ -0,0 +1,92 @@
+﻿$Path = '.\Bin\NVIDIA-ALEXIS-78\ccminer.exe'
+$Uri = 'https://github.com/Sniffdog/miners/raw/master/ccmineralexis_78.zip'
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Algorithms = [PSCustomObject]@{
+    #Lyra2z = 'lyra2z'
+    #Equihash = 'equihash' #not supported
+    #Cryptonight = 'cryptonight'
+    #Ethash = 'ethash' #not supported
+    #Sia = 'sia'
+    #Yescrypt = 'yescrypt'
+    #BlakeVanilla = 'vanilla'
+    #Lyra2RE2 = 'lyra2v2'
+    Skein = 'skein'
+    #Qubit = 'qubit'
+    #NeoScrypt = 'neoscrypt'
+    #X11 = 'x11'
+    #MyriadGroestl = 'myr-gr'
+    #Groestl = 'groestl'
+    #Keccak = 'keccak'
+    #Scrypt = 'scrypt'
+    #Bitcore = 'bitcore'
+    Blake2s = 'blake2s'
+    #Sib = 'sib'
+    #X17 = 'x17'
+    #Quark = 'quark'
+    #Hmq1725 = 'hmq1725'
+    #Veltor = 'veltor'
+    #X11evo = 'x11evo'
+    #Timetravel = 'timetravel'
+    #Blakecoin = 'blakecoin'
+    Lbry = 'lbry'
+    #Jha = 'jha'
+    #Skunk = 'skunk'
+    #Tribus = 'tribus'
+    #Phi = 'phi'
+    #Hsr = 'hsr'
+    #Polytimos = 'poly'
+}
+
+$Optimizations = [PSCustomObject]@{
+    Lyra2z = ' --api-remote --api-allow=0/0'
+    Equihash = ''
+    Cryptonight = ' --api-remote --api-allow=0/0'
+    Ethash = ''
+    Sia = ''
+    Yescrypt = ''
+    BlakeVanilla = ''
+    Lyra2RE2 = ''
+    Skein = ' -i 28 --api-remote'
+    Qubit = ''
+    NeoScrypt = ''
+    X11 = ''
+    MyriadGroestl = ''
+    Groestl = ''
+    Keccak = ''
+    Scrypt = ''
+    Bitcore = ' --api-remote --api-allow=0/0'
+    Blake2s = ''
+    Sib = ''
+    X17 = ''
+    Quark = ''
+    Hmq1725 = ' --api-remote --api-allow=0/0'
+    Veltor = ''
+    X11evo = ''
+    Timetravel = ' --api-remote --api-allow=0/0'
+    Blakecoin = ''
+    Lbry = ''
+    Jha = ' --api-remote --api-allow=0/0'
+    Skunk = ' --api-remote --api-allow=0/0'
+    Tribus = ' --api-remote --api-allow=0/0'
+    Phi = ' --api-remote --api-allow=0/0'
+    Hsr = ' --api-remote --api-allow=0/0'
+    Polytimos = '' 
+    
+}
+
+$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = 'NVIDIA'
+        Path = $Path
+        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -u $($Pools.', $_, '.User) -p $($Pools.', $_, '.Pass)', $Optimizations.$_)
+        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
+        API = 'Ccminer'
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+        PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
+        PrerequisiteURI = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+    }
+}
