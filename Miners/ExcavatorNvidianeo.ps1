d0279c435ad48582aae3649d2aacfdba65cc500a
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-02-02 00:36:03 -0700

    Update ExcavatorNvidianeo.ps1

--- Miners/ExcavatorNvidianeo.ps1
+++ Miners/ExcavatorNvidianeo.ps1
@@ -7,7 +7,7 @@ $Uri = "https://github.com/nicehash/excavator/releases/download/v1.4.2a/excavato
 
 $Commands = [PSCustomObject]@{
     
-    #"neoscrypt" = @() #Neoscrypt
+    #"neoscrypt" = @() 
     
 }
 
