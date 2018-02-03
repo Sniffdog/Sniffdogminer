732d96ab5311031f1a3e1db99e7cb94421979139
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-02-02 23:51:31 -0700

    Update ExcavatorNvidia.ps1

--- Miners/ExcavatorNvidia.ps1
+++ Miners/ExcavatorNvidia.ps1
@@ -3,7 +3,7 @@
 $Threads = 3
 
 $Path = ".\Bin\Excavator\excavator.exe"
-$Uri = "https://github.com/nicehash/excavator/releases/download/v1.4.2a/excavator_v1.4.2a_NVIDIA_Win64.zip"
+$Uri = "https://github.com/nicehash/excavator/releases/download/v1.4.3a/excavator_v1.4.3a_NVIDIA_Win64.zip"
 
 $Commands = [PSCustomObject]@{
     #"equihash" = @() 
