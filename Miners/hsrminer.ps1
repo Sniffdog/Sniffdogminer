d0b7a4c92a5efdcfb2e11a4009b773fc8378e103
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-02-02 22:35:55 -0600

    Add files via upload

---
+++ Miners/hsrminer.ps1
@@ -0,0 +1,59 @@
+﻿$Path = '.\Bin\NVIDIA-hsrminer\hsrminer.exe'
+$Uri = 'https://github.com/justaminer/hsrm-fork/raw/master/hsrminer_neoscrypt_fork.zip'
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Algorithms = [PSCustomObject]@{
+    #Lyra2z = 'lyra2z' #not supported
+    #Equihash = 'equihash' #not supported
+    #Cryptonight = 'cryptonight' #not supported
+    #Ethash = 'ethash' #not supported
+    #Sia = 'sia' #use TpruvoT
+    #Yescrypt = 'yescrypt' #use TpruvoT
+    #BlakeVanilla = 'vanilla'
+    #Lyra2RE2 = 'lyra2v2' 
+    #Skein = 'skein' #use TpruvoT
+    #Qubit = 'qubit' #use TpruvoT
+    NeoScrypt = 'neoscrypt'
+    #X11 = 'x11' #use TpruvoT
+    #MyriadGroestl = 'myr-gr'
+    #Groestl = 'groestl'
+    #Keccak = 'keccak' 
+    #Scrypt = 'scrypt' #use TpruvoT
+    #Nist5 = 'nist5'
+}
+
+$Optimizations = [PSCustomObject]@{
+    Lyra2z = ''
+    Equihash = ''
+    Cryptonight = ''
+    Ethash = ''
+    Sia = ''
+    Yescrypt = ''
+    BlakeVanilla = ''
+    Lyra2RE2 = ''
+    Skein = ''
+    Qubit = ''
+    NeoScrypt = ''
+    X11 = ''
+    MyriadGroestl = ''
+    Groestl = ''
+    Keccak = ''
+    Scrypt = ''
+    Nist5 = ''
+}
+
+$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = 'NVIDIA'
+        Path = $Path
+        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -u $($Pools.', $_, '.User) -p $($Pools.', $_, '.Pass)', $Optimizations.$_)
+        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
+        API = 'Ccminer'
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+        PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
+        PrerequisiteURI = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+    }
+}
