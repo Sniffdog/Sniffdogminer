011aaaaff930757cef389b236aa18129afa3a103
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-02-03 13:30:19 -0700

    Update RemoveLogs.bat

--- RemoveLogs.bat
+++ RemoveLogs.bat
@@ -2,8 +2,6 @@
 cd /d %~dp0
 set /p execute= This process will delete all unnecessary log files created by the miners and SniffDog to free up space. Are you sure you want to continue? [Y/N] 
 IF /I "%execute%"=="Y" (
-	if exist "Bin\Cryptonight-Claymore\*_log.txt" del "Bin\Cryptonight-Claymore\*_log.txt"
-	if exist "Bin\Equihash-Claymore\*_log.txt" del "Bin\Equihash-Claymore\*_log.txt"
 	if exist "Bin\Ethash-Claymore\*_log.txt" del "Bin\Ethash-Claymore\*_log.txt"
 	for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "Logs\*.txt"') do @del "Logs\%%F"
 	ECHO All existing log files have been successfully deleted.
