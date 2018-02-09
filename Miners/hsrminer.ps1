d8e4b9d16425365d25ecbcc3fcdfd2280a0c672f
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-02-08 22:45:52 -0700

    Update hsrminer.ps1

--- Miners/hsrminer.ps1
+++ Miners/hsrminer.ps1
@@ -1,5 +1,5 @@
-﻿$Path = '.\Bin\NVIDIAHsrminer\hsrminer_neoscrypt_fork.exe'
-$Uri = 'https://github.com/justaminer/hsrm-fork/raw/master/hsrminer_neoscrypt_fork.zip'
+﻿$Path = '.\Bin\NVIDIAHsrminer\hsrminer_neoscrypt_fork_hp.exe'
+$Uri = 'https://github.com/justaminer/hsrm-fork/raw/master/hsrminer_neoscrypt_fork_hp.zip'
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
