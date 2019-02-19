18058be2322cecd7dc6f2021373b521e1b8f4ebd
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2019-02-19 12:26:59 -0700

    Update Gminer.ps1

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -1,5 +1,5 @@
 $Path = ".\Bin\NVIDIA-Gminer\\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.32/gminer_1_32_minimal_windows64.zip"
+$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.33/gminer_1_33_minimal_windows64.zip"
 $Commands = [PSCustomObject]@{
     #"bitcore" = "" #Bitcore
     #"blake2s" = "" 
