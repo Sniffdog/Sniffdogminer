4299f40b892e81e8c88ebe4d0a41ba1cec56490b
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2019-07-07 13:46:47 -0600

    Update Gminer.ps1

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -1,37 +1,15 @@
 $Path = ".\Bin\NVIDIA-Gminer\\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.36/gminer_1_36_minimal_windows64.zip"
+$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.50/gminer_1_50_windows64.zip"
 $Commands = [PSCustomObject]@{
-    #"bitcore" = "" #Bitcore
-    #"blake2s" = "" 
-    #"blakecoin" = "" #Blakecoin
-    #"vanilla" = "" #BlakeVanilla
-    #"cryptonight" = "" #Cryptonight
-    #"decred" = "" #Decred
+    
     "equihash" = '' #Equihash
     "equihash192" = '192_7' #Equihash192
     "equihash96" = '96_5' #Equihash96
     "equihash144" = '144_5' #Equihash144
-    #"ethash" = "" #Ethash
-    #"groestl" = "" #Groestl
-    #"hmq1725" = "" #hmq1725
-    #"keccak" = "" #Keccak
-    #"lbry" = "" #Lbry
-    #"lyra2v2" = "" #Lyra2RE2
-    #"lyra2z" = "" #Lyra2z
-    #"myr-gr" = "" #MyriadGroestl
-    #"neoscrypt" = "" #NeoScrypt
-    #"nist5" = "" #Nist5
-    #"pascal" = "" #Pascal
-    #"qubit" = "" #Qubit
-    #"scrypt" = "" #Scrypt
-    #"sia" = "" #Sia
-    #"sib" = "" #Sib
-    #"skein" = "" #Skein
-    #"timetravel" = "" #Timetravel
-    #"x11" = "" #X11
-    #"x11evo" = "" #X11evo
-    #"x17" = "" #X17
-    #"yescrypt" = "" #Yescrypt
+    "equihash150" = '150_5' #Equihash150
+    "equihash210" = '210_9' #Equihash210
+    
+    
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
