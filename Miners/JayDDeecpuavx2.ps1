3fa0ecde4a1fc1d05763b9b94af618135f38e671
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2019-07-07 13:57:56 -0600

    Update JayDDeecpuavx2.ps1

--- Miners/JayDDeecpuavx2.ps1
+++ Miners/JayDDeecpuavx2.ps1
@@ -1,5 +1,5 @@
 ﻿$Path = ".\Bin\CPU-JayDDee\cpuminer-avx2.exe"
-$Uri = "https://github.com/JayDDee/cpuminer-opt/files/1996977/cpuminer-opt-3.8.8.1-windows.zip"
+$Uri = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.9.5.2/cpuminer-opt-3.9.5.2-windows.zip"
 
 $Commands = [PSCustomObject]@{
     #"bitcore" = "" #Bitcore
